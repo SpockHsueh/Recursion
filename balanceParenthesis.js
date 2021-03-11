7c81418e90b7f79d11e0c38acabe154378020dbf
Spock <>
2021-03-11 16:38:35 +0800

    Balance Parenthesis

---
+++ balanceParenthesis.js
@@ -0,0 +1,28 @@
+function balanced(testVariable, startIndex = 0, currentIndex = 0) {
+  if (startIndex == testVariable.length) {
+    return currentIndex == 0
+  }
+
+  if (currentIndex < 0) {
+    return false
+  }
+
+  if (testVariable[startIndex] == "(") { 
+    return balanced(testVariable, startIndex + 1, currentIndex + 1)
+  }
+
+  // Recursive case2
+  else if (testVariable[startIndex] == ")") { 
+    return  balanced(testVariable, startIndex + 1, currentIndex - 1)
+  }
+
+  else {
+    return false
+  }
+}
+
+testVariable = ["(", "(", ")", ")", "(", ")"]
+console.log("The array [\"(\", \"(\", \")\", \")\", \"(\", \")\"] is balanced: " + balanced(testVariable))
+
+testVariable = ["(", "(", ")"]
+console.log("The array [\"(\", \"(\", \")\"] is balanced: " + balanced(testVariable))
